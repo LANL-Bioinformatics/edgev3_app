65564f4029098fbf2291b157f38abb537653aeac
chien-chi <andylo@pn2405853.lanl.gov>
2026-07-21 10:57:03 -0600

    update edgev3 with health check endpoint

--- docker_build.sh
+++ docker_build.sh
@@ -25,7 +25,7 @@ build_and_save_if_missing() {
 
 docker_build() {
     # edgev3 
-    build_and_save_if_missing "edgev3:20260713" "linux/$ARCH" "docker_images/Dockerfile.edgev3" "docker_images/edgev3_20260713_$ARCH.tgz" "edgev3"
+    build_and_save_if_missing "edgev3:20260721" "linux/$ARCH" "docker_images/Dockerfile.edgev3" "docker_images/edgev3_20260721_$ARCH.tgz" "edgev3"
 
     # edgev3-nextflow
     build_and_save_if_missing "edgev3-nextflow:20260615" "linux/$ARCH" "docker_images/Dockerfile.edgev3-nextflow" "docker_images/edgev3-nextflow_20260615_$ARCH.tgz" "edgev3-nextflow"
--- edgev3_app.sh
+++ edgev3_app.sh
@@ -258,7 +258,7 @@ prepare_runtime_config() {
 IMAGES=(
     "nginx nginx_latest_$ARCH.tgz latest"
     "edgev3-nextflow edgev3-nextflow_20260615_$ARCH.tgz 20260615"
-    "edgev3 edgev3_20260713_$ARCH.tgz 20260713"
+    "edgev3 edgev3_20260721_$ARCH.tgz 20260721"
     "edgev3-mongo edgev3-mongo_20260721_$ARCH.tgz 20260721"
 )
 
